c673228af111929c90ec31c36038cd82d5bc3c9a
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2018-11-01 20:12:33 +0100

    Step 4b: renamed test methods

--- src/test/java/fr/ath/kata/multicurrency/MultiCurrencyTest.java
+++ src/test/java/fr/ath/kata/multicurrency/MultiCurrencyTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MultiCurrencyTest {
 
     @Test
-    public void should_return_ten_dollars_when_asked_twice_five_dollars () {
+    public void should_test_multiplication() {
         Dollar fiveDollars = new Dollar(5);
 
         assertThat(fiveDollars.times(2)).isEqualTo(new Dollar(10));
@@ -16,7 +16,7 @@ public class MultiCurrencyTest {
     }
 
     @Test
-    public void shoud_two_dollars_with_same_amount_be_equal() {
+    public void should_test_equality() {
         assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
         assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
