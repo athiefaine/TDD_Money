69d8cff4034e0bf757967af0b4b7acf3c6fdcebb
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2018-11-01 16:52:14 +0100

    Second step: get rid of Dollar side-effect

--- src/test/java/fr/ath/kata/multicurrency/MultiCurrencyTest.java
+++ src/test/java/fr/ath/kata/multicurrency/MultiCurrencyTest.java
@@ -7,14 +7,13 @@ public class MultiCurrencyTest {
 
     @Test
     public void should_return_ten_dollars_when_asked_twice_five_dollars () {
-        // GIVEN
         Dollar fiveDollars = new Dollar(5);
 
-        //WHEN
-        fiveDollars.times(2);
+        Dollar tenDollars = fiveDollars.times(2);
+        Assertions.assertThat(tenDollars.amount).isEqualTo(10);
 
-        //THEN
-        Assertions.assertThat(fiveDollars.amount).isEqualTo(10);
+        Dollar fifteenDollars = fiveDollars.times(3);
+        Assertions.assertThat(fifteenDollars.amount).isEqualTo(15);
 
     }
 
--- src/test/todo/test_todolist.md
+++ src/test/todo/test_todolist.md
@@ -1,5 +1,5 @@
 * $5 + 10 € = $10 if rate is 2:1
 * ~~$5 * 2 = $10~~
 * amount must be private
-* side-effects for Dollar, it is mutable ?
+* ~~side-effects for Dollar, it is mutable ?~~
 * money rounding ?
